cae68f03e20596f1ad4ef6bbb45ccbb04a277f3a
joaocarlos8 <joaocarlostrapasson88@gmail.com>
2025-05-22 22:03:26 -0300

    TRABALHO GRANSHOP

---
+++ GranShopAPI/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo {
+        Title = "GranShop",
+        Version = "v1",
+        Description= "Documentação da API"
+    });
+});
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Granshop API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
